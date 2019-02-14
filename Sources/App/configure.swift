836da97a79b3f7a57ea47ba21aa3c633c09c6a27
swelsh1984 <stuartwelsh@qtsgroup.com>
2019-02-14 14:50:34 +0000

    Adding back in ENV details.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,7 +20,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Configure a PostgreSQL database
     
     var databases = DatabasesConfig()
-    let hostname = /*Environment.get("DATABASE_HOSTNAME") ??*/ "localhost"
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
