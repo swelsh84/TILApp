08b9234728c749606f259efed3dcc43d8df181d5
swelsh1984 <stuartwelsh@qtsgroup.com>
2019-02-18 09:49:25 +0000

    Add basic website

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -21,4 +21,7 @@ public func routes(_ router: Router) throws {
     
     let categoriesController = CategoriesController()
     try router.register(collection: categoriesController)
+    
+    let websiteController = WebsiteController()
+    try router.register(collection: websiteController)
 }
